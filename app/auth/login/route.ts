87c9e882d5f3425a6c381782802bf48594062a3b
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-14 12:03:26 -0600

    feat: Add proxy/middleware with JWT based auth

--- app/auth/login/route.ts
+++ app/auth/login/route.ts
@@ -1,10 +1,25 @@
+import "dotenv";
 import { verify } from "@/lib/auth";
 import { User } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import { SignJWT } from "jose";
 
 export async function POST(request: NextRequest) {
     const formData = await request.formData();
     const user = await User.findOne({ email: formData.get("email") });
     if (!user || !await verify(formData.get("password")?.toString()!, user.password!)) return new NextResponse(null, { status: 401 });
-    return new NextResponse(null, { status: 200 });
+    const expiryTime = +process.env.JWT_EXPIRY_TIME!;
+    const token = await new SignJWT({ email: user.email })
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime(process.env.JWT_EXPIRY_TIME!)
+        .sign(new TextEncoder().encode(process.env.JWT_SECRET!))
+    const response = new NextResponse(null, { status: 200 });
+    response.cookies.set("token", token, {
+        secure: true,
+        maxAge: expiryTime,
+        httpOnly: true,
+        sameSite: "strict",
+    })
+    return response;
 }

21ee761ad739395d9d021db56ef8afb9f72cb611
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-17 19:37:43 -0600

    feat: Add email verification and remove default users

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,25 +2,35 @@ import { betterAuth } from "better-auth";
 import db from "./db";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from '../auth-schema';
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  secure: false,
+  port: 587,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  }
+});
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: { ...schema }
   }),
-  emailAndPassword: {
-    enabled: true,
-    minPasswordLength: 1,
+  emailAndPassword: { enabled: true },
+  emailVerification: {
+    async sendVerificationEmail({ user, url }) {
+      await transporter.sendMail({
+        from: '"AutoCAM" <ishan.karmakar24@gmail.com>',
+        to: user.email,
+        subject: "Verify your AutoCAM email!",
+        text: `Click the link to verify your email: ${url}`
+      })
+    },
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
   },
   baseURL: "http://localhost:3000"
 })
-
-const DEFAULT_USERS = [
-  { email: "valor@gmail.com", name: "Valor", password: "6800" },
-  { email: "ftc@gmail.com", name: "FTC", password: "viperbots" }
-]
-for (const user of DEFAULT_USERS) {
-  try {
-    await auth.api.signUpEmail({ body: user });
-  } catch { }
-}

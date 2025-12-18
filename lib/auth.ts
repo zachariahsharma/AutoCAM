a30ecd959a81df0ae4443c5ef0d9a26e55124574
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-17 20:06:29 -0600

    feat: Make sending email more extensible

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,17 +2,7 @@ import { betterAuth } from "better-auth";
 import db from "./db";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from '../auth-schema';
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  secure: false,
-  port: 587,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  }
-});
+import sendMail from "./mailer";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -22,7 +12,7 @@ export const auth = betterAuth({
   emailAndPassword: { enabled: true },
   emailVerification: {
     async sendVerificationEmail({ user, url }) {
-      await transporter.sendMail({
+      await sendMail({
         from: '"AutoCAM" <ishan.karmakar24@gmail.com>',
         to: user.email,
         subject: "Verify your AutoCAM email!",

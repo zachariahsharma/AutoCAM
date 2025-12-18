bd390523d3fc3bf71d81ff249b95494c4072fef5
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-18 15:15:57 -0600

    feat: Send email for team invite

--- app/api/team/[id]/invite/route.ts
+++ app/api/team/[id]/invite/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 import { Props } from "../route";
 import db from "@/lib/db";
+import mailer from "@/lib/mailer";
+import { TeamInvites } from "@/lib/schema/entities";
 
 export async function POST(req: NextRequest, { params }: Props) {
     const teamId = Number((await params).id);
@@ -9,10 +11,24 @@ export async function POST(req: NextRequest, { params }: Props) {
     if (!session.user.emailVerified) return new NextResponse(null, { status: 403 });
 
     const teamMember = await db.query.TeamMembers.findFirst({
+        with: { team: true },
         where: (table, {eq}) => eq(table.team_id, teamId) && eq(table.user_id, session.user.id)
     });
     if (!teamMember || !teamMember.admin) return new NextResponse(null, { status: 403 });
+    
+    const email = (await req.formData()).get("email")?.toString();
+    if (!email) return new NextResponse(null, { status: 400 });
+    const [invite] = await db.insert(TeamInvites).values({
+        team_id: teamId,
+        email: email
+    }).returning({ id: TeamInvites.id });
 
-    const collaborator = (await req.formData()).get("collaborator")?.toString();
-    if (!collaborator) return new NextResponse(null, { status: 400 });
+    await mailer.sendMail({
+        from: '"AutoCAM" <ishan.karmakar24@gmail.com>',
+        to: email,
+        subject: `Join ${teamMember.team.name}`,
+        text: `Join the ${teamMember.team.name} Team with this link: http://localhost:3000/team/accept/${invite.id}`
+    });
+
+    return new NextResponse(null, { status: 200 });
 }

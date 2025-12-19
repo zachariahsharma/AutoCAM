8b8057dc12b080bb940cb15a40730f61e1f3d5a6
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-19 14:45:07 -0600

    feat: Move DELETE for runners

--- app/api/runners/route.ts
+++ app/api/runners/route.ts
@@ -1,23 +1,12 @@
 import db from "@/lib/db";
-import { TeamRunners } from "@/lib/schema/entities";
-import { count, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-const RunnerTokenInvalid = new NextResponse(null, { status: 401 });
-
-function getRunnerToken(req: NextRequest) {
+export const RunnerTokenInvalid = new NextResponse(null, { status: 401 });
+export function getRunnerToken(req: NextRequest) {
   const authHeader = req.headers.get("authorization");
   if (!authHeader) return null;
   const token = authHeader.split("Bearer ")[1];
   return token;
-}
-
-export async function DELETE(req: NextRequest) {
-  const token = getRunnerToken(req);
-  if (!token) return RunnerTokenInvalid;
-  const numDeleted = await db.delete(TeamRunners).where(eq(TeamRunners.token, token)).returning({ count: count() });
-  if (numDeleted[0].count == 0) return RunnerTokenInvalid;
-  return new NextResponse(null, { status: 204 });
 }
 
 export async function GET(req: NextRequest) {
--- app/api/teams/[id]/runners/route.ts
+++ app/api/teams/[id]/runners/route.ts
@@ -4,6 +4,8 @@ import db from "@/lib/db";
 import { EmailNotVerifiedResponse, isEmailVerified } from "@/lib/auth";
 import { getTeamMember, TeamMemberNotAdmin } from "../../route";
 import { TeamRunners } from "@/lib/schema/entities";
+import { getRunnerToken, RunnerTokenInvalid } from "@/app/api/runners/route";
+import { and, count, eq } from "drizzle-orm";
 
 export async function GET(req: NextRequest, { params }: Props) {
   const teamId = Number((await params).id);
@@ -24,4 +26,18 @@ export async function POST(req: NextRequest, { params }: Props) {
 
   const runner = await db.insert(TeamRunners).values({ team_id, name }).returning({ token: TeamRunners.token });
   return NextResponse.json(runner[0], { status: 201 });
+}
+
+export async function DELETE(req: NextRequest, { params }: Props) {
+  const teamId = Number((await params).id);
+  if (!await isEmailVerified(req)) return EmailNotVerifiedResponse;
+  const teamMember = await getTeamMember(req, teamId);
+  if (!teamMember || !teamMember.admin) return TeamMemberNotAdmin;
+  const token = getRunnerToken(req);
+  if (!token) return RunnerTokenInvalid;
+  const numDeleted = await db.delete(TeamRunners)
+    .where(and(eq(TeamRunners.token, token), eq(TeamRunners.team_id, teamId)))
+    .returning({ count: count() });
+  if (numDeleted[0].count == 0) return RunnerTokenInvalid;
+  return new NextResponse(null, { status: 204 });
 }

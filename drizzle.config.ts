b3b36adb19e70c9dbd059a63e1c55702899925bc
Zachariah K. Sharma <33808784+zachariahsharma@users.noreply.github.com>
2025-12-19 21:14:51 -0600

    drizzle password

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   schema: './lib/schema/*',
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!
+    url: process.env.DATABASE_URL!,
+    password: process.env.DB_PASSWORD!,
   }
 });

399fe1d2a0bd33833fbc868ad9dabfb6d7749ce2
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-20 20:03:56 -0600

    feat: Got team invites working with RLS

--- app/api/teams/[id]/invite/route.ts
+++ app/api/teams/[id]/invite/route.ts
@@ -1,27 +1,43 @@
-import { EmailNotVerifiedResponse, isEmailVerified } from "@/lib/auth";
+import { auth, EmailNotVerifiedResponse, isEmailVerified } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 import { Props } from "../route";
-import db from "@/lib/db";
+import db, { withUser } from "@/lib/db";
 import mailer from "@/lib/mailer";
-import { TeamInvites } from "@/lib/schema/entities";
-import { getTeamMember, TeamMemberNotAdmin } from "../../route";
+import { TeamInvites, Teams } from "@/lib/schema/entities";
+import { headers } from "next/headers";
+import { eq } from "drizzle-orm";
+import { DatabaseError } from "pg";
 
 export async function POST(req: NextRequest, { params }: Props) {
   const team_id = Number((await params).id);
-  if (!await isEmailVerified(req)) return EmailNotVerifiedResponse;
-  const teamMember = await getTeamMember(req, team_id);
-  if (!teamMember || !teamMember.admin) return TeamMemberNotAdmin;
+  const session = (await auth.api.getSession({ headers: await headers() }))!;
+  if (!await isEmailVerified()) return EmailNotVerifiedResponse;
 
   const email = (await req.formData()).get("email")?.toString();
   if (!email) return new NextResponse(null, { status: 400 });
-  const [invite] = await db.insert(TeamInvites).values({ team_id, email }).returning({ id: TeamInvites.id });
 
-  await mailer.sendMail({
-    from: '"AutoCAM" <ishan.karmakar24@gmail.com>',
-    to: email,
-    subject: `Join ${teamMember.team.name}`,
-    text: `Join the ${teamMember.team.name} Team with this link: http://${process.env.BASE_URL}/api/teams/accept/${invite.id}`
-  });
-
-  return new NextResponse(null, { status: 200 });
+  try {
+    const [id, teamName] = await withUser(session.user.id, async tx => {
+      const [invite] = await tx
+        .insert(TeamInvites)
+        .values({ team_id, email })
+        .returning({ id: TeamInvites.id });
+      // Unless there is an egregious race condition this should never return nothing
+      const team = (await tx.query.Teams.findFirst({
+        where: eq(Teams.id, team_id)
+      }))!;
+      return [invite.id, team.name];
+    });
+    await mailer.sendMail({
+      from: '"AutoCAM" <ishan.karmakar24@gmail.com>',
+      to: email,
+      subject: `Join ${teamName}`,
+      text: `Join the ${teamName} Team with this link: ${new URL(`/api/teams/accept/${id}`, `http://${process.env.BASE_URL}`)}`
+    });
+    return new NextResponse(null, { status: 200 });
+  } catch (err) {
+    if (err instanceof DatabaseError && err.code === "42501")
+      return new NextResponse(null, { status: 403 });
+    throw err;
+  }
 }

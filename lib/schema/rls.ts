c4384843caeb04801fbe6897e5c7710e28823035
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-27 22:07:50 -0500

    feat: More changes

--- lib/schema/rls.ts
+++ lib/schema/rls.ts
@@ -1,6 +1,6 @@
 import { and, eq, getTableName, SQL, sql } from "drizzle-orm"
 import { TeamKeys, TeamMembers } from "./entities"
-import { Machines, Materials, PartCategories, Parts, PartsToPlates, Plates, ToolMachines, ToolMaterials, Tools } from "./cam"
+import { BoxTubeJobs, BoxTubes, Machines, Materials, PartCategories, Parts, PartsToPlates, PlateJobs, Plates, ToolMachines, ToolMaterials, Tools } from "./cam"
 
 export function UserId() {
   return sql`current_setting('app.user_id', true)`
@@ -68,13 +68,22 @@ export function TeamFromPlate(pid: any) {
   `
 }
 
+export function TeamFromBoxTube(bid: any) {
+  return sql`
+  (
+    SELECT ${BoxTubes.team_id}
+    FROM ${sql.identifier(getTableName(BoxTubes))}
+    WHERE ${eq(BoxTubes.id, bid)}
+  )
+  `
+}
+
 export function CheckPartsPlatesTeam(): SQL<boolean> {
   return sql`
   EXISTS (
     SELECT 1 FROM ${Plates}
     INNER JOIN ${Parts} ON ${eq(Parts.id, PartsToPlates.part_id)}
-    WHERE ${eq(Plates.id, PartsToPlates.plate_id)}
-      AND ${eq(Plates.category_id, Parts.category_id)}
+    WHERE ${and(eq(Plates.id, PartsToPlates.plate_id), eq(Plates.category_id, Parts.category_id))}
   )
   `
 }
@@ -84,8 +93,7 @@ export function CheckToolMaterialsTeam(): SQL<boolean> {
   EXISTS (
     SELECT 1 FROM ${Tools}
     INNER JOIN ${Materials} ON ${eq(Materials.id, ToolMaterials.material_id)}
-    WHERE ${eq(Tools.id, ToolMaterials.tool_id)}
-      AND ${eq(Tools.team_id, Materials.team_id)}
+    WHERE ${and(eq(Tools.id, ToolMaterials.tool_id), eq(Tools.team_id, Materials.team_id))}
   )
   `
 }
@@ -95,8 +103,37 @@ export function CheckToolMachinesTeam(): SQL<boolean> {
   EXISTS (
     SELECT 1 FROM ${Tools}
     INNER JOIN ${Machines} ON ${eq(Machines.id, ToolMachines.machine_id)}
-    WHERE ${eq(Tools.id, ToolMachines.tool_id)}
-      AND ${eq(Tools.team_id, Machines.team_id)}
+    WHERE ${and(eq(Tools.id, ToolMachines.tool_id), eq(Tools.team_id, Machines.team_id))}
+  )
+  `
+}
+
+export function CheckPlateJobsTeams(): SQL<boolean> {
+  return sql`
+  EXISTS (
+    SELECT 1 FROM ${Plates}
+    INNER JOIN ${Tools} ON ${eq(Tools.id, PlateJobs.tool_id)}
+    INNER JOIN ${Machines} ON ${eq(Machines.id, PlateJobs.machine_id)}
+    WHERE ${and(
+      eq(Plates.id, PlateJobs.plate_id),
+      eq(TeamFromCategory(Plates.category_id), Machines.team_id),
+      eq(Machines.team_id, Tools.team_id)
+    )}
+  )
+  `
+}
+
+export function CheckBoxTubeJobsTeams(): SQL<boolean> {
+  return sql`
+  EXISTS (
+    SELECT 1 FROM ${BoxTubes}
+    INNER JOIN ${Tools} ON ${eq(Tools.id, BoxTubeJobs.tool_id)}
+    INNER JOIN ${Machines} ON ${eq(Machines.id, BoxTubeJobs.machine_id)}
+    WHERE ${and(
+      eq(BoxTubes.id, BoxTubeJobs.box_tube_id),
+      eq(BoxTubes.team_id, Machines.team_id),
+      eq(Machines.team_id, Tools.team_id)
+    )}
   )
   `
 }

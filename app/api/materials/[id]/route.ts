32f3f785fcbfd35a9aac97aec701b926a16c0851
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-29 21:51:06 -0500

    feat: Materials PATCH/DELETE routes

---
+++ app/api/materials/[id]/route.ts
@@ -0,0 +1,14 @@
+import { parseJsonBody, routeFactory } from "@/lib/api-utils";
+import { Materials } from "@/lib/schema/cam";
+import { eq } from "drizzle-orm";
+import { createUpdateSchema } from "drizzle-zod";
+
+export const PATCH = routeFactory(async (req, authType, tx, id) => {
+  const body = await parseJsonBody(await req.json(), createUpdateSchema(Materials));
+
+  return await tx.update(Materials).set(body).where(eq(Materials.id, id));
+});
+
+export const DELETE = routeFactory(
+  (req, authType, tx, id) => tx.delete(Materials).where(eq(Materials.id, id))
+);

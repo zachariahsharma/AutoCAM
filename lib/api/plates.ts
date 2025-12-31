8d3bd6f432fd9129677490e438ce3a470200a4ba
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-31 11:36:01 -0600

    feat: Plates API written

--- lib/api/plates.ts
+++ lib/api/plates.ts
@@ -1,32 +1,61 @@
-import "@/lib/openapi/registry";
 import { Plates } from "@/lib/db/schema/cam";
 import { createInsertSchema, createSelectSchema, createUpdateSchema } from "drizzle-zod";
 import zod from "zod";
 import { registry } from "@/lib/openapi/registry";
+import { apiKey, userSession } from "./auth";
+import { scopeNames as scopes } from "../scopes";
 
-export const PlatesCreateSchema = createInsertSchema(Plates).openapi("PlatesCreate");
-export const PlatesUpdateSchema = createUpdateSchema(Plates).openapi("PlatesUpdate");
-export const PlatesGetSchema = createSelectSchema(Plates).openapi("PlatesGet");
+export const PlatesCreateSchema = createInsertSchema(Plates).omit({ category_id: true });
+export const PlatesUpdateSchema = createUpdateSchema(Plates).omit({ category_id: true });
+const Plate = createSelectSchema(Plates).omit({ category_id: true }).meta({ id: "Plate" });
 
-// OpenAPI route definitions
 registry.registerPath({
-  method: "patch",
-  path: "/api/plates/{id}",
+  method: "get",
+  path: "/api/pc/{id}/plates",
   tags: ["Plates"],
-  description: "Update a plate",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.plates.read] }
+  ],
+  summary: "Get Plates",
   request: {
-    params: zod.object({ id: zod.number() }),
+    params: zod.object({ id: zod.number().meta({ description: "ID of the part category" }) })
+  },
+  responses: {
+    200: {
+      description: "This endpoint returns the plates from the given part category",
+      content: {
+        "application/json": {
+          schema: zod.array(Plate)
+        }
+      }
+    }
+  }
+});
+
+registry.registerPath({
+  method: "post",
+  path: "/api/pc/{id}/plates",
+  tags: ["Plates"],
+  summary: "Create Plate",
+  description: "This endpoint requires the user's email to be verified",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.plates.write] }
+  ],
+  request: {
+    params: zod.object({ id: zod.number().meta({ description: "ID of the part category" }) }),
     body: {
       content: {
         "application/json": {
-          schema: PlatesUpdateSchema
+          schema: PlatesCreateSchema
         }
       }
     }
   },
   responses: {
-    200: {
-      description: "Plate updated",
+    201: {
+      description: "Returns the ID of the created plate",
       content: {
         "application/json": {
           schema: zod.object({ id: zod.number() })
@@ -37,21 +66,48 @@ registry.registerPath({
 });
 
 registry.registerPath({
+  method: "patch",
+  path: "/api/plates/{id}",
+  tags: ["Plates"],
+  summary: "Update Plate",
+  description: "This endpoint requires the user's email to be verified",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.plates.write] }
+  ],
+  request: {
+    params: zod.object({ id: zod.number().meta({ description: "ID of the plate" }) }),
+    body: {
+      content: {
+        "application/json": {
+          schema: PlatesUpdateSchema
+        }
+      }
+    }
+  },
+  responses: {
+    204: {
+      description: "Plate updated successfully",
+    }
+  }
+});
+
+registry.registerPath({
   method: "delete",
   path: "/api/plates/{id}",
   tags: ["Plates"],
-  description: "Delete a plate",
+  summary: "Delete Plate",
+  description: "This endpoint requires the user's email to be verified",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.plates.write] }
+  ],
   request: {
-    params: zod.object({ id: zod.number() }),
+    params: zod.object({ id: zod.number().meta({ description: "ID of the plate" }) }),
   },
   responses: {
-    200: {
-      description: "Plate deleted",
-      content: {
-        "application/json": {
-          schema: zod.object({ id: zod.number() })
-        }
-      }
+    204: {
+      description: "Plate deleted successfully",
     }
   }
 });

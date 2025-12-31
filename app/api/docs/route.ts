7c10dfdbcf06975d50381549bd7ebb9cc3b17edc
Ishan Karmakar <ishan.karmakar24@gmail.com>
2025-12-30 19:05:02 -0600

    feat: Refactor Scalar UI to serve both sources

--- app/api/docs/route.ts
+++ app/api/docs/route.ts
@@ -1,15 +1,8 @@
-import { OpenApiGeneratorV31 } from "@asteasolutions/zod-to-openapi";
-import { registry } from "@/lib/openapi/registry";
 import { ApiReference } from "@scalar/nextjs-api-reference";
-import "@/lib/api";
 
-const generator = new OpenApiGeneratorV31(registry.definitions);
 export const GET = ApiReference({
-  content: JSON.stringify(generator.generateDocument({
-    openapi: "3.1.2",
-    info: {
-      version: "1.0.0",
-      title: "AutoCAM"
-    }
-  }))
+  sources: [
+    { url: "/api/docs/openapi", title: "AutoCAM" },
+    { url: "/api/auth/open-api/generate-schema", title: "Auth" }
+  ]
 });

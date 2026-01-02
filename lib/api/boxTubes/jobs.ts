d69cd158832b6742ca5d026de540122a7c72103b
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-01-01 19:14:49 -0600

    feat: Box tube jobs documentation

--- lib/api/boxTubes/jobs.ts
+++ lib/api/boxTubes/jobs.ts
@@ -3,9 +3,95 @@ import { parseJsonBody, routeFactory, routeResponse } from "..";
 import { eq } from "drizzle-orm";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import zod from "zod";
+import { registry } from "@/lib/openapi/registry";
+import { CommonAuthorization, Conflict, NotFound, ValidationError } from "../common";
+import { apiKey, userSession } from "../auth";
+import { scopeNames as scopes } from "@/lib/scopes";
 
 const CreateSchema = createInsertSchema(BoxTubeJobs).omit({ box_tube_id: true });
-const Job = createSelectSchema(BoxTubeJobs).omit({ box_tube_id: true, machine_id: true, tool_id: true });
+const Job = createSelectSchema(BoxTubeJobs).omit({ box_tube_id: true, machine_id: true, tool_id: true }).openapi("Box Tube Job");
+
+registry.registerPath({
+  method: "get",
+  path: "/api/boxTubes/{id}/jobs",
+  tags: ["Box Tube Jobs"],
+  summary: "Get Box Tube Jobs",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.boxTubes.jobs.read] }
+  ],
+  request: {
+    params: zod.object({ id: zod.number().openapi({ description: "Box tube ID" }) })
+  },
+  responses: {
+    200: {
+      description: "This endpoint returns the box tube jobs for a given box tube",
+      content: {
+        "application/json": {
+          schema: zod.array(Job)
+        }
+      }
+    },
+    ...CommonAuthorization,
+    ...ValidationError
+  }
+});
+
+registry.registerPath({
+  method: "post",
+  path: "/api/boxTubes/{id}/jobs",
+  tags: ["Box Tube Jobs"],
+  summary: "Create Box Tube Job",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.boxTubes.jobs.write] }
+  ],
+  request: {
+    params: zod.object({ id: zod.number().openapi({ description: "Box tube ID" }) }),
+    body: {
+      content: {
+        "application/json": {
+          schema: CreateSchema
+        }
+      }
+    }
+  },
+  responses: {
+    201: {
+      description: "Returns the ID of the created box tube",
+      content: {
+        "application/json": {
+          schema: zod.object({ id: zod.number() })
+        }
+      }
+    },
+    ...CommonAuthorization,
+    ...ValidationError,
+    ...Conflict
+  }
+});
+
+registry.registerPath({
+  method: "delete",
+  path: "/api/boxTubes/jobs/{id}",
+  tags: ["Box Tube Jobs"],
+  summary: "Delete Box Tube Job",
+  security: [
+    { [userSession.name]: [] },
+    { [apiKey.name]: [scopes.boxTubes.jobs.write] }
+  ],
+  request: {
+    params: zod.object({ id: zod.number().openapi({ description: "Box tube job ID" }) })
+  },
+  responses: {
+    204: {
+      description: "Box tube job deleted successfully"
+    },
+    ...CommonAuthorization,
+    ...ValidationError,
+    ...NotFound
+  }
+})
 
 export const GET = routeFactory(async (req, authType, tx, id) => {
   if (!id) return routeResponse(422);

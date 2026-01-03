147c78dd842c67dcf90460ee441f312dceff04f1
Zachariah K. Sharma <zachariah.sharma@gmail.com>
2026-01-03 12:27:12 -0600

    Adjust Quanitities

--- app/dashboard/@tabs/settings/layout.tsx
+++ app/dashboard/@tabs/settings/layout.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { Team } from "../types";
+import { Team } from "@/app/types";
 import { useSelectedLayoutSegment } from "next/navigation";
 export function useCurrentTab() {
   const segment = useSelectedLayoutSegment("tabs");

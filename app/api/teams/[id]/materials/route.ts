93b7c8e21f59cebf796af96f1749694fe61480a2
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-01-03 22:50:26 -0600

    fix: Materials route

--- app/api/teams/[id]/materials/route.ts
+++ app/api/teams/[id]/materials/route.ts
@@ -1,5 +1,1 @@
-import { createMaterial, getMaterials } from "@/app/api/materials/route";
-import { routeFactory } from "@/lib/api";
-
-export const GET = routeFactory((req, authType, tx, id) => getMaterials(authType, tx, id));
-export const POST = routeFactory(async (req, authType, tx, id) => createMaterial(authType, tx, await req.json(), id));
+export { GET, POST } from "@/lib/api/materials";

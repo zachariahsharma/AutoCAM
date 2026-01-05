3f49fa787ad11af8a2dcb82785c621aa02d74aa8
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-01-05 17:35:51 -0600

    feat: Add validation of API key scopes to route factory

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,5 +1,5 @@
 import { auth, AuthType, getKeyDigest } from "@/lib/auth/server";
-import { DrizzleQueryError } from "drizzle-orm";
+import { and, DrizzleQueryError, eq, inArray, sql } from "drizzle-orm";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { DatabaseError } from "pg";
@@ -19,6 +19,7 @@ import "./user";
 import "./auth";
 import "./tools";
 import "./jobs";
+import { TeamKeys } from "../db/schema/entities";
 
 /**
  * Get authenticated user ID only (for operations that require email verification)
@@ -119,7 +120,6 @@ export function handleDatabaseError(err: unknown): NextResponse {
   if (err instanceof DrizzleQueryError)
     cause = err.cause as DatabaseError;
   if (cause instanceof DatabaseError) {
-    if (cause.code === "42501") return routeResponse(403, { message: "Permission denied" }); // Permission denied
     if (cause.code === "23505") return routeResponse(409); // Unique violation
   }
   throw err;
@@ -137,17 +137,29 @@ export function checkAnyChanges(records: any[]) {
 
 export interface RouteFactoryConfig {
   emailVerifiedNeeded?: boolean;
+  requiredScopes?: string[];
 }
 
 export type RouteFactoryCallback = (req: NextRequest, authType: AuthType, tx: Transaction, id: number | null) => Promise<any>;
 
 export function routeFactory(callback: RouteFactoryCallback, config?: RouteFactoryConfig) {
+  config ??= {};
   return async (req: NextRequest, { params }: { params: Promise<{ id: string } | {} | undefined> }) => {
     const authType = await getAuthType();
-    try { await validateAuthType(authType, config?.emailVerifiedNeeded ?? false); }
+    try { await validateAuthType(authType, config.emailVerifiedNeeded ?? false); }
     catch (err) { return err; }
-
+    
     return withAuth(authType, async tx => {
+      if (authType.keyDigest) {
+        if (!config.requiredScopes) throw routeResponse(403, { message: "API Keys are not authorized to use this endpoint" });
+        const result = await tx.query.TeamKeys.findFirst({
+          where: and(
+            eq(TeamKeys.digest, authType.keyDigest),
+            sql`${TeamKeys.scopes} @> ${config.requiredScopes}`
+          )
+        });
+        if (!result) throw routeResponse(403, { message: "API Key does not have the required scopes for this endpoint" })
+      }
       try {
         let id = null;
         const p = await params;

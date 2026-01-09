8bffb4fabad539e84f752fb6386dd502dd3ba5af
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-01-09 08:41:45 -0600

    fix: Accidentally used parseJsonBody instead of parseSchema

--- lib/api/jobs.ts
+++ lib/api/jobs.ts
@@ -61,7 +61,7 @@ export const SingleGET = routeFactory(async (req, authType, tx, id) => {
   await checkUserTeam(tx, authType, job.team_id);
   return routeResponse(200, {
     // FIXME: Move file into schema and move parseJsonBody up a level
-    ...await parseJsonBody(job, Job),
+    ...await parseSchema(job, Job),
     file: await getSignedUrl(client, new GetObjectCommand({
       Bucket: process.env.AUTOCAM_BUCKET,
       Key: `teams/${job.team_id}/jobs/${id}`
@@ -118,7 +118,3 @@ export const DELETE = routeFactory(async (req, authType, tx, id) => {
     Key: `teams/${job.team_id}/jobs/${id}`
   }));
 }, { user: { emailVerified: true }, apiKey: { scopes: [scopes.jobs.delete] } });
-function parseJsonBody(job: { id: number; created_at: Date; team_id: number; kind: "plate:arrange" | "plate:cam" | "box_tube"; claimed_by: string | null; payload: unknown; response: unknown; }, Job: zod.ZodPipe<zod.ZodObject<{ id: zod.ZodInt; created_at: zod.ZodDate; kind: zod.ZodEnum<{ "plate:arrange": "plate:arrange"; "plate:cam": "plate:cam"; box_tube: "box_tube"; }>; claimed_by: zod.ZodNullable<zod.ZodString>; payload: zod.ZodType<Json, unknown, zod.core.$ZodTypeInternals<Json, unknown>>; response: zod.ZodNullable<zod.ZodType<Json, unknown, zod.core.$ZodTypeInternals<Json, unknown>>>; queue_position: zod.ZodNumber; }, { out: {}; in: {}; }>, zod.ZodTransform<{ status: string; id: number; created_at: Date; kind: "plate:arrange" | "plate:cam" | "box_tube"; claimed_by: string | null; payload: Json; response: Json; queue_position: number; }, { id: number; created_at: Date; kind: "plate:arrange" | "plate:cam" | "box_tube"; claimed_by: string | null; payload: Json; response: Json; queue_position: number; }>>): object | PromiseLike<object | undefined> | undefined {
-  throw new Error("Function not implemented.");
-}
-

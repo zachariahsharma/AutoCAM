820b6081473fd00dab524eceb0292a3e6d762e12
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-01-22 00:34:15 -0600

    feat: Add create, update, and get routes for materials

--- lib/api/materials.ts
+++ lib/api/materials.ts
@@ -3,93 +3,13 @@ import { createInsertSchema, createSelectSchema, createUpdateSchema } from "driz
 import zod from "zod";
 import { registry } from "@/lib/openapi/registry";
 import { apiKey, userSession } from "./auth";
-import { scopeNames as scopes } from "../scopes";
-import { checkUserTeam, CommonAuthorization, Conflict, IDPolicy, parseSchema, registerTeamEndpoint, routeFactory, routeResponse, ValidationError } from "./common";
-import { teamIdFromDigest } from "../auth/server";
+import { scopeNames, scopeNames as scopes } from "../scopes";
+import { checkUserTeam, checkUserTeamTRPC, CommonAuthorization, routeFactory, routeResponse, ValidationError } from "./common";
+import { teamIdFromDigestTRPC } from "../auth/server";
 import { eq } from "drizzle-orm";
-
-const CreateSchema = createInsertSchema(Materials).omit({ team_id: true });
-const UpdateSchema = createUpdateSchema(Materials).omit({ team_id: true });
-const Material = createSelectSchema(Materials).omit({ team_id: true }).openapi("Material")
-
-registerTeamEndpoint([scopes.materials.read], {
-  method: "get",
-  path: "/api/materials",
-  tags: ["Materials"],
-  summary: "Get Materials",
-  responses: {
-    200: {
-      description: "This endpoint returns the materials from the given team",
-      content: {
-        "application/json": {
-          schema: zod.array(Material)
-        }
-      }
-    },
-    ...CommonAuthorization,
-  }
-});
-
-registerTeamEndpoint([scopes.materials.write], {
-  method: "post",
-  path: "/api/materials",
-  tags: ["Materials"],
-  summary: "Create Material",
-  description: "This endpoint requires the user's email to be verified",
-  request: {
-    body: {
-      content: {
-        "multipart/form-data": {
-          schema: zod.object({
-            data: CreateSchema.openapi({ description: "Material info as stringified JSON" }),
-            file: zod.instanceof(File).openapi({ type: "string", format: "binary", description: "Material file upload" })
-          })
-        }
-      }
-    }
-  },
-  responses: {
-    201: {
-      description: "Returns the ID of the created material",
-      content: {
-        "application/json": {
-          schema: zod.object({ id: zod.number() })
-        }
-      }
-    },
-    ...CommonAuthorization,
-    ...ValidationError,
-    ...Conflict
-  }
-});
-
-registry.registerPath({
-  method: "patch",
-  path: "/api/materials/{id}",
-  tags: ["Materials"],
-  summary: "Update Material",
-  security: [
-    { [userSession.name]: [] },
-    { [apiKey.name]: [scopes.materials.write] }
-  ],
-  request: {
-    params: zod.object({ id: zod.number().openapi({ description: "ID of the material" }) }),
-    body: {
-      content: {
-        "application/json": {
-          schema: UpdateSchema
-        }
-      }
-    }
-  },
-  responses: {
-    204: {
-      description: "Material successfully updated",
-    },
-    ...CommonAuthorization,
-    ...ValidationError
-  }
-});
+import { procedure, router } from "../trpc/server";
+import db from "../db";
+import { TRPCError } from "@trpc/server";
 
 registry.registerPath({
   method: "delete",
@@ -112,46 +32,58 @@ registry.registerPath({
   }
 });
 
-export const GET = routeFactory(async (req, authType, tx, id) => {
-  id ??= await teamIdFromDigest(tx, authType);
-  await checkUserTeam(tx, authType, id);
-  return routeResponse(200, await parseSchema(await tx.query.Materials.findMany({
-    where: eq(Materials.team_id, id)
-  }), zod.array(Material)));
-}, {
-  user: { idPolicy: IDPolicy.Required },
-  apiKey: { scopes: [scopes.materials.read], idPolicy: IDPolicy.Forbidden }
+export default router({
+  get: procedure.meta({
+    openapi: { method: "GET", path: "/api/trpc/materials.get" },
+    authStrategies: ["user", "apiKey"],
+    scopes: [scopeNames.materials.read]
+  }).input(zod.number().optional())
+    .output(zod.array(createSelectSchema(Materials).omit({ team_id: true })))
+    .query(async opts => {
+      opts.input ??= await teamIdFromDigestTRPC(opts.ctx.apiKey!);
+      return await db.query.Materials.findMany({ where: eq(Materials.team_id, opts.input) });
+    }),
+
+  get_single: procedure.meta({
+    openapi: { method: "GET", path: "/api/trpc/materials.get_single" },
+    authStrategies: ["user", "apiKey"],
+    scopes: [scopeNames.materials.read],
+  }).input(zod.number())
+    .output(createSelectSchema(Materials).omit({ team_id: true }))
+    .query(async opts => {
+      const material = await db.query.Materials.findFirst({ where: eq(Materials.id, opts.input) });
+      if (!material) throw new TRPCError({ code: "NOT_FOUND" });
+      await checkUserTeamTRPC(opts.ctx, material.team_id);
+      return material;
+    }),
+
+  create: procedure.meta({
+    openapi: { method: "POST", path: "/api/trpc/materials.create" },
+    authStrategies: ["user", "apiKey"],
+    scopes: [scopeNames.materials.write],
+    emailVerified: true
+  }).input(createInsertSchema(Materials).extend({ team_id: zod.number().optional() })).output(zod.number())
+    .mutation(async opts => {
+      opts.input.team_id ??= await teamIdFromDigestTRPC(opts.ctx.apiKey!);
+      await checkUserTeamTRPC(opts.ctx, opts.input.team_id, true);
+      const team_id = opts.input.team_id!;
+      const [id] = await db.insert(Materials).values({ ...opts.input, team_id }).returning({ id: Materials.id });
+      return id.id;
+    }),
+
+  update: procedure.meta({
+      openapi: { method: "POST", path: "/api/trpc/materials.update" },
+      authStrategies: ["user", "apiKey"],
+      scopes: [scopeNames.materials.write],
+      emailVerified: true
+    }).input(createUpdateSchema(Materials).extend({ id: zod.number() }))
+    .mutation(async opts => {
+      const material = await db.query.Materials.findFirst({ where: eq(Materials.id, opts.input.id) });
+      if (!material) throw new TRPCError({ code: "NOT_FOUND" });
+      await checkUserTeamTRPC(opts.ctx, material.team_id, true);
+      await db.update(Materials).set(opts.input).where(eq(Materials.id, opts.input.id));
+    }),
 });
-
-export const SingleGET = routeFactory(async (req, authType, tx, id) => {
-  if (!id) return routeResponse(422);
-  const material = await tx.query.Materials.findFirst({ where: eq(Materials.id, id) });
-  if (!material) return routeResponse(404);
-  await checkUserTeam(tx, authType, material.team_id);
-  return routeResponse(200, await parseSchema(material, Material));
-}, { user: {}, apiKey: { scopes: [scopes.materials.read] } });
-
-export const POST = routeFactory(async (req, authType, tx, team_id) => {
-  team_id ??= await teamIdFromDigest(tx, authType);
-  await checkUserTeam(tx, authType, team_id, true);
-  const body = await parseSchema(await req.json(), CreateSchema);
-
-  const [id] = await tx.insert(Materials).values({ ...body, team_id }).returning({ id: Materials.id });
-  return routeResponse(201, id);
-}, {
-  user: { emailVerified: true, idPolicy: IDPolicy.Required },
-  apiKey: { scopes: [scopes.materials.write], idPolicy: IDPolicy.Forbidden }
-});
-
-export const PATCH = routeFactory(async (req, authType, tx, id) => {
-  if (!id) return routeResponse(422);
-  const material = await tx.query.Materials.findFirst({ where: eq(Materials.id, id) });
-  await checkUserTeam(tx, authType, material?.team_id, true);
-  const body = await parseSchema(await req.json(), createUpdateSchema(Materials));
-
-  const result = await tx.update(Materials).set(body).where(eq(Materials.id, id));
-  return routeResponse(result.rowCount === 0 ? 404 : 204);
-}, { user: { emailVerified: true }, apiKey: { scopes: [scopes.materials.write] } });
 
 export const DELETE = routeFactory(async (req, authType, tx, id) => {
   if (!id) return routeResponse(422);

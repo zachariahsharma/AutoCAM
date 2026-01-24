ec0560d9b5a7e9016829a8a2703ad59251b31a6c
Ishan Karmakar <ishan.karmakar24@gmail.com>
2026-01-24 10:42:35 -0600

    feat: Update and delete route for part categories

--- lib/api/pc/index.ts
+++ lib/api/pc/index.ts
@@ -1,66 +1,14 @@
 import './assignments';
 import { createInsertSchema, createSelectSchema, createUpdateSchema } from "drizzle-zod";
-import { registry } from "../../openapi/registry";
-import { apiKey, userSession } from "../auth";
 import { PartCategories } from "../../db/schema/cam";
-import { scopeNames, scopeNames as scopes } from "../../scopes";
+import { scopeNames } from "../../scopes";
 import zod from "zod";
-import { checkUserTeam, checkUserTeamTRPC, CommonAuthorization, Conflict, IDPolicy, parseSchema, registerTeamEndpoint, routeFactory, routeResponse, s3DeleteWithPrefix, ValidationError } from "../common";
-import { teamIdFromDigest, teamIdFromDigestTRPC } from "../../auth/server";
+import { checkUserTeamTRPC, s3DeleteWithPrefix } from "../common";
+import { teamIdFromDigestTRPC } from "../../auth/server";
 import { and, eq } from "drizzle-orm";
 import { procedure, router } from '@/lib/trpc/server';
 import db from '@/lib/db';
-
-const UpdateSchema = createUpdateSchema(PartCategories).omit({ team_id: true });
-
-registry.registerPath({
-  method: "patch",
-  path: "/api/pc/{id}",
-  tags: ["Part Categories"],
-  summary: "Update Part Category",
-  security: [
-    { [userSession.name]: [] },
-    { [apiKey.name]: [scopes.pc.write] }
-  ],
-  request: {
-    params: zod.object({ id: zod.number().openapi({ description: "ID of the part category" }) }),
-    body: {
-      content: {
-        "application/json": {
-          schema: UpdateSchema
-        }
-      }
-    }
-  },
-  responses: {
-    204: {
-      description: "Part category successfully updated",
-    },
-    ...CommonAuthorization,
-    ...ValidationError
-  }
-});
-
-registry.registerPath({
-  method: "delete",
-  path: "/api/pc/{id}",
-  tags: ["Part Categories"],
-  summary: "Delete Part Category",
-  security: [
-    { [userSession.name]: [] },
-    { [apiKey.name]: [scopes.pc.write] }
-  ],
-  request: {
-    params: zod.object({ id: zod.number().openapi({ description: "ID of the part category" }) }),
-  },
-  responses: {
-    204: {
-      description: "Part category successfully deleted",
-    },
-    ...CommonAuthorization,
-    ...ValidationError
-  }
-});
+import { TRPCError } from '@trpc/server';
 
 export default router({
   get: procedure.meta({
@@ -86,7 +34,8 @@ export default router({
   create: procedure.meta({
     openapi: { method: "POST", path: "/api/trpc/pc.create" },
     authStrategies: ["user", "apiKey"],
-    scopes: [scopeNames.pc.write]
+    scopes: [scopeNames.pc.write],
+    emailVerified: true
   }).input(createInsertSchema(PartCategories).extend({ team_id: zod.number().optional() }))
     .output(zod.number()).mutation(async opts => {
       const team_id = opts.input.team_id ?? await teamIdFromDigestTRPC(opts.ctx.apiKey!);
@@ -94,22 +43,31 @@ export default router({
 
       const [id] = await db.insert(PartCategories).values({ ...opts.input, team_id }).returning({ id: PartCategories.id });
       return id.id;
-    })
+    }),
+
+  update: procedure.meta({
+    openapi: { method: "POST", path: "/api/trpc/pc.update" },
+    authStrategies: ["user", "apiKey"],
+    scopes: [scopeNames.pc.write],
+    emailVerified: true
+  }).input(createUpdateSchema(PartCategories).extend({ id: zod.number() }).omit({ team_id: true }))
+    .mutation(async opts => {
+      const pc = await db.query.PartCategories.findFirst({ where: eq(PartCategories.id, opts.input.id) });
+      if (!pc) return new TRPCError({ code: "NOT_FOUND" });
+      await checkUserTeamTRPC(opts.ctx, pc.team_id);
+      await db.update(PartCategories).set(opts.input).where(eq(PartCategories.id, opts.input.id));
+    }),
+
+  delete: procedure.meta({
+    openapi: { method: "POST", path: "/api/trpc/pc.delete" },
+    authStrategies: ["user", "apiKey"],
+    scopes: [scopeNames.pc.write],
+    emailVerified: true
+  }).input(zod.number()).mutation(async opts => {
+    const pc = await db.query.PartCategories.findFirst({ where: eq(PartCategories.id, opts.input) });
+    if (!pc) return new TRPCError({ code: "NOT_FOUND" });
+    await checkUserTeamTRPC(opts.ctx, pc.team_id);
+    await db.delete(PartCategories).where(eq(PartCategories.id, opts.input));
+    await s3DeleteWithPrefix(`teams/${pc.team_id}/pc/${opts.input}/`);
+  })
 });
-
-export const PATCH = routeFactory(async (req, authType, tx, id) => {
-  if (!id) return routeResponse(422);
-  const pc = await tx.query.PartCategories.findFirst({ where: eq(PartCategories.id, id) });
-  await checkUserTeam(tx, authType, pc?.team_id);
-  const body = await parseSchema(await req.json(), UpdateSchema);
-  await tx.update(PartCategories).set(body).where(eq(PartCategories.id, id)).returning({ id: PartCategories.id })
-}, { user: { emailVerified: true }, apiKey: { scopes: [scopes.pc.write] } });
-
-export const DELETE = routeFactory(async (req, authType, tx, id) => {
-  if (!id) return routeResponse(422);
-  const pc = await tx.query.PartCategories.findFirst({ where: eq(PartCategories.id, id) });
-  if (!pc) return routeResponse(404);
-  await checkUserTeam(tx, authType, pc.team_id);
-  await tx.delete(PartCategories).where(eq(PartCategories.id, id));
-  await s3DeleteWithPrefix(`teams/${pc.team_id}/pc/${id}/`);
-}, { user: { emailVerified: true }, apiKey: { scopes: [scopes.pc.write] } });
